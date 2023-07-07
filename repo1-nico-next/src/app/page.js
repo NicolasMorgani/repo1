5d76e3624180418a6b7b1eab07f9abfc913115b2
NicoMorgani <115804234+NicoMorgani@users.noreply.github.com>
2023-07-07 14:50:16 -0300

    logos

--- repo1-nico-next/src/app/page.js
+++ repo1-nico-next/src/app/page.js
@@ -12,13 +12,13 @@ export default function Home() {
     <>
 
       <div className={styles.container} >
-     
+
 
 
         <Container  >
           <Row>
             <Col style={{ display: "flex" }} data-aos="fade-up"  >
-              <Card border="primary" style={{  width: '50rem', background: "none",marginTop:30 }}>
+              <Card border="primary" style={{ width: '50rem', background: "none", marginTop: 30 }}>
                 <Card.Body>
                   <h1>Descargate la App</h1>
                   <p className={styles.textohero}>
@@ -30,23 +30,23 @@ export default function Home() {
 
 
           </Row>
-          <Row style={{  height: "40px" }}>
+          <Row style={{ height: "40px" }}>
 
           </Row>
 
 
           <Row>
             <Col data-aos="zoom-in-up">
-           
-              <VideoPlayerp1  videoSrc="/avion1.mp4" />
-             
+
+              <VideoPlayerp1 videoSrc="/avion1.mp4" />
+
             </Col>
 
           </Row>
           <Row >
-            <Col style={{ display: "flex", justifyContent: "center" }}data-aos="zoom-in">
-            
-                  <Imgislascard />
+            <Col style={{ display: "flex", justifyContent: "center" }} data-aos="zoom-in">
+
+              <Imgislascard />
 
 
             </Col>
@@ -57,7 +57,7 @@ export default function Home() {
                 <Card border="info" style={{ width: '20rem', backgroundColor: "black" }}>
                   <Card.Img variant="top" src="menu1.jpg" />
                   <Card.Body >
-                    <Card.Title style={{ color:"#0deff7",fontFamily: "cursive" }}>Digital e Interactivo</Card.Title>
+                    <Card.Title style={{ color: "#0deff7", fontFamily: "cursive" }}>Digital e Interactivo</Card.Title>
                     <Card.Text>
                       Descargate y experimenta con la App distintas modalidades de juego
                       con una experiencia totalmente digital e interactiva sobre las Islas Malvinas
@@ -69,7 +69,7 @@ export default function Home() {
                 <Card border="info" style={{ width: '20rem', backgroundColor: "black" }}>
                   <Card.Img variant="top" src="mapa.jpg" />
                   <Card.Body>
-                    <Card.Title style={{color:"#0deff7",fontFamily: "cursive"}}>Didactico e Intuitivo</Card.Title>
+                    <Card.Title style={{ color: "#0deff7", fontFamily: "cursive" }}>Didactico e Intuitivo</Card.Title>
                     <Card.Text>
                       Descubre acontecimientos que ocurrieron desde los
                       inicios hasta los tiempos actuales y explora los dias del conflicto de 1982
@@ -81,7 +81,7 @@ export default function Home() {
                 <Card border="info" style={{ width: '20rem', backgroundColor: "black" }}>
                   <Card.Img variant="top" src="barco.jpg" />
                   <Card.Body>
-                    <Card.Title style={{color:"#0deff7",fontFamily: "cursive"}} >Divertido y didactico</Card.Title>
+                    <Card.Title style={{ color: "#0deff7", fontFamily: "cursive" }} >Divertido y didactico</Card.Title>
                     <Card.Text>
                       Diviertete mientras exploras y experimentas a travez de la App
                       y sus diferentes funcionalidades y modos de juego
@@ -93,15 +93,45 @@ export default function Home() {
                 <Card border="info" style={{ width: '20rem', backgroundColor: "black" }}>
                   <Card.Img variant="top" src="quiz11.jpg" />
                   <Card.Body>
-                    <Card.Title style={{color:"#0deff7",fontFamily: "cursive"}}>Adaptable y para todos</Card.Title>
+                    <Card.Title style={{ color: "#0deff7", fontFamily: "cursive" }}>Adaptable y para todos</Card.Title>
                     <Card.Text>
                       Contenido desarrollado para dispositvos moviles con informacion y canales utiles
-                      para el Veterano de Guerra de Malvinas VGM 
+                      para el Veterano de Guerra de Malvinas VGM
                     </Card.Text>
                   </Card.Body>
                 </Card>
               </Col>
             </Row>
+            <Row style={{ height: "70px" }}>
+
+          </Row>
+          <Row>
+            <Col style={{ display: "flex", justifyContent: "center", marginTop: "30px", textAlign: "center", alignItems: "center" }} data-aos="fade-down">
+              <h2 style={{ color: "lightseagreen", fontFamily: "unset", fontSize: "3rem", fontWeight: "bold" }} >Desarrollamos la Web</h2>
+            </Col>
+          </Row>
+          <Row style={{ height: "70px" }}>
+
+          </Row>
+            <Row className="justify-content-center " style={{alignItems:"center"}}>
+              
+                <Card style={{ width: '10rem',background:"none" }}>
+                  <Card.Img variant="top" src="React.png" />
+                </Card>
+             
+             
+              <Card style={{ width: '20rem',background:"none"}}>
+                  <Card.Img variant="top" src="Nextblanco.png" />
+                </Card>
+                <Card style={{ width: '10rem',background:"none" }}>
+                  <Card.Img variant="top" src="Visual_Studio_Cod.png" />
+                </Card>
+                <Card style={{ width: '10rem',background:"none" }}>
+                  <Card.Img variant="top" src="Bootstrap.png" />
+                </Card>
+           </Row>
+          
+           
           </Container>
           <Row style={{ height: "50px" }}>
 
